4aba9985e3eef4d55265ffcb2b116fb2ab3fefc1
Nadir-Fablab <72986640+Nadir-Fablab@users.noreply.github.com>
2022-01-26 19:15:37 +0500

    ultraSonic code added

---
+++ UltraSonic arduino code/ultrasonic.ino
@@ -0,0 +1,36 @@
+const int trigPin = 11;    // Trigger
+const int echoPin = 12;    // Echo
+long duration, cm;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  //pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343
+  //inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135
+  
+  //Serial.print(inches);
+  //Serial.print("in, ");
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+}
